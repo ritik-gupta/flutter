33d703ace7b666f0841d21e04ae4ef82e1dcdc7e
Ritik Gupta <ritikg8126@gmail.com>
2020-06-04 18:27:09 +0530

    Insert an appbar

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello World!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('This is an AppBar'),
+        ),
+        body: Text('Hello World'),
+      ),
+    );
   }
 }

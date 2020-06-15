594c16b77050fd0ba888e63908026c78d8ccb274
Ritik Gupta <ritikg8126@gmail.com>
2020-06-15 20:06:53 +0530

    Let's add some real questions

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -15,16 +15,94 @@ class _MyAppState extends State<MyApp> {
   var _qIndex = 0;
   static const _questions = [
     {
-      'questionText': 'What\'s your favourite colour?',
-      'answers': ['Black', 'Blue', 'Red', 'Green']
+      'questionText': 'Ctrl, Shift and Alt are called ____ keys?',
+      'answers': [
+        {'text': 'Modifier', 'score': 10},
+        {'text': 'function', 'score': 0},
+        {'text': 'alphanumeric', 'score': 0},
+        {'text': 'adjustment', 'score': 0}
+      ]
     },
     {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'questionText': 'MS-Word is an example of _____',
+      'answers': [
+        {'text': 'An operating system', 'score': 0},
+        {'text': 'A processing device', 'score': 0},
+        {'text': 'Application software', 'score': 10},
+        {'text': 'An input device', 'score': 0}
+      ]
     },
     {
-      'questionText': 'What\'s your favourite season?',
-      'answers': ['Summer', 'Winter', 'Rainy', 'Spring'],
+      'questionText': 'A computer cannot "boot" if it does not have the _____',
+      'answers': [
+        {'text': 'Compiler', 'score': 0},
+        {'text': 'Loader', 'score': 0},
+        {'text': 'Operating system', 'score': 10},
+        {'text': 'Assembler', 'score': 0}
+      ]
+    },
+    {
+      'questionText': '________ is the process of dividing the disk into tracks and sectors',
+      'answers': [
+        {'text': 'Tracking', 'score': 0},
+        {'text': 'Formatting', 'score': 10},
+        {'text': 'Crashing', 'score': 0},
+        {'text': 'Allotting', 'score': 0}
+      ]
+    },
+    {
+      'questionText': '_____are attempts by individuals to obtain confidential information from you by falsifying their identity',
+      'answers': [
+        {'text': 'Phishing trips', 'score': 0},
+        {'text': 'Phishing scams', 'score': 10},
+        {'text': 'Computer viruses', 'score': 0},
+        {'text': 'Spyware scams', 'score': 0}
+      ]
+    },
+    {
+      'questionText': 'Junk e-mail is also called ______',
+      'answers': [
+        {'text': 'Spam', 'score': 10},
+        {'text': 'Spoof', 'score': 0},
+        {'text': 'Sniffer Script', 'score': 0},
+        {'text': 'Spool', 'score': 0}
+      ]
+    },
+    {
+      'questionText': 'Microsoft Office is an example of a ______',
+      'answers': [
+        {'text': 'Closed source software', 'score': 0},
+        {'text': 'Open source software', 'score': 0},
+        {'text': 'Horizontal market software', 'score': 10},
+        {'text': 'vertical market software', 'score': 0}
+      ]
+    },
+    {
+      'questionText': 'By default, your documents print in ________ mode',
+      'answers': [
+        {'text': 'Landscape', 'score': 0},
+        {'text': 'Page Setup', 'score': 0},
+        {'text': 'Portrait', 'score': 10},
+        {'text': 'Print View', 'score': 0}
+      ]
+    },
+    {
+      'questionText': 'The first computer was programmed using _____',
+      'answers': [
+        {'text': 'Assembly language', 'score': 0},
+        {'text': 'Spaghetti code', 'score': 0},
+        {'text': 'Source code', 'score': 0}
+        {'text': 'Machine language', 'score': 10},
+      ]
+    },
+    {
+      'questionText': 'Storage capacity of magnetic disk depends on ______',
+      'answers': [
+        {'text': 'disk pack in disk surface', 'score': 0},
+        {'text': 'bits per inch of tracks', 'score': 0},
+        {'text': 'tracks per inch of surface', 'score': 0},
+        {'text': 'All of the above', 'score': 10}
+      ]
     },
   ];
 

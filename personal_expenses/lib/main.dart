08702311037944cdd5b4213aab5fb90f82c646f2
Ritik Gupta <ritikg8126@gmail.com>
2020-06-28 21:58:09 +0530

    add dummy transaction widget

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -29,9 +30,49 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('List OF transactions'),
-          ),
+          Column(
+            children: transactions.map((txn) {
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      txn.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                      ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        txn.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        )
+                        ),
+                      Text(
+                        txn.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey
+                        ),
+                      )
+                  ])
+                ],
+              ));
+            }).toList(),
+          )
         ],
       ),
     );
